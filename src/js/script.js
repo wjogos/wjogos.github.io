0607ebcb8d20c46d0294eb5a6cd3ecbfd9bf52a4
kiartes <thaissouzabr4@gmail.com>
2025-01-03 20:36:25 +0000

    play

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+function open_fullscreen() {
+    var elem = document.getElementById("game-area");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
